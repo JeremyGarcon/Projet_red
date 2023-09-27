13d9b4b4ca50a4e7f81ca223b6cac05d2994c40d
unknown <jeremy.garcon@ynov.com>
2023-09-27 14:47:52 +0200

    ajustement

--- src_jeremy/jeu.go
+++ src_jeremy/jeu.go
@@ -1,15 +1,14 @@
 package main
 
-import "os/exec"
-
 func main() {
-	playMusic()
+	// playMusic() en cours
 	var joueurAsPersonnage bool = false //variable qui permet de savoir si le joueur a un personnage
 	var Pointeur_joueurAsPersonnage *bool = &joueurAsPersonnage
 	chargement_jeu()                        //charge le jeu
 	Menu_Start(Pointeur_joueurAsPersonnage) //affiche le menu de depart
 }
 
+/*
 func playMusic() {
 	// Utilisez "wmplayer.exe" pour Windows avec le fichier MP3 à la racine
 	cmd := exec.Command("wmplayer.exe", "musique.mp3")
@@ -19,3 +18,4 @@ func playMusic() {
 		panic(err)
 	}
 }
+*/
