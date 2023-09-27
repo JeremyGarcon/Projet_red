18faec38a066a5466627f4763983ba232873f045
tyfenn <tyfenn.ledevehat@ynov.com>
2023-09-27 14:46:28 +0200

    marchand

--- src_tyf/marchand.go
+++ src_tyf/marchand.go
@@ -1,36 +1,73 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
 
-type Marchand struct {
-	Inventaire  []Inventaire
-
+// Structure représentant l'inventaire du joueur
+type Inventory struct {
+    Items map[string]int
 }
 
-
-
-
-for {
-	fmt.Println("Menu:")
-	fmt.Println("1. Utiliser une Potion de poison")
-	fmt.Println("2. Acheter une Potion de poison auprès du marchand")
-	fmt.Println("3. Quitter")
-
-	var choix int
-	fmt.Print("Choisissez une option: ")
-	fmt.Scanln(&choix)
-
-	switch choix {
-	case 1:
-		// Utilisation de la Potion de poison
-		poisonPot(personnage)
-	case 2:
-		// Achat d'une Potion de poison auprès du marchand
-		acheterPotion(personnage, marchand)
-	case 3:
-		fmt.Println("Au revoir!")
-		return // Quitter le programme
-	default:
-		fmt.Println("Option invalide. Veuillez choisir une option valide.")
-	}
+// Fonction pour ajouter un article à l'inventaire
+func (inv *Inventory) AddItem(item Item) {
+    if inv.Items == nil {
+        inv.Items = make(map[string]int)
+    }
+    inv.Items[item.Name]++
 }
+
+// Fonction pour retirer un article de l'inventaire
+func (inv *Inventory) RemoveItem(item Item) {
+    if count, ok := inv.Items[item.Name]; ok && count > 0 {
+        inv.Items[item.Name]--
+    }
+}
+
+func main() {
+    // Créer un inventaire vide pour le joueur
+    playerInventory := &Inventory{}
+
+    // Créer un marchand
+    merchant := []Item{
+        {"Potion de vie", 0},
+    }
+
+    // Afficher le menu du marchand
+    fmt.Println("Bienvenue chez le marchand ! Voici ce qu'il vend :")
+    for i, item := range merchant {
+        fmt.Printf("%d. %s (Prix: %d pièces d'or)\n", i+1, item.Name, item.Price)
+    }
+
+    // Demander au joueur de faire un choix
+    var choice int
+    fmt.Print("Veuillez choisir un article à acheter (0 pour quitter) : ")
+    fmt.Scan(&choice)
+
+    // Vérifier si le choix est valide
+    if choice < 0 || choice > len(merchant) {
+        fmt.Println("Choix invalide.")
+        return
+    }
+
+    // Acheter l'article choisi
+    if choice == 0 {
+        fmt.Println("Merci d'être passé chez le marchand.")
+    } else {
+        itemToBuy := merchant[choice-1]
+        if itemToBuy.Price > 0 {
+            fmt.Printf("Vous avez acheté %s.\n", itemToBuy.Name)
+            playerInventory.AddItem(itemToBuy)
+        } else {
+            fmt.Printf("Vous avez reçu gratuitement %s.\n", itemToBuy.Name)
+            playerInventory.AddItem(itemToBuy)
+        }
+    }
+
+    // Afficher l'inventaire mis à jour du joueur
+    fmt.Println("Votre inventaire :")
+    for item, count := range playerInventory.Items {
+        fmt.Printf("%s: %d\n", item, count)
+    }
+}
+
