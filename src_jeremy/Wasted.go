51a132638a2ba9d1779d53325ec04dfadd052c87
unknown <jeremy.garcon@ynov.com>
2023-09-28 11:19:48 +0200

    Wasted modif

--- src_jeremy/Wasted.go
+++ src_jeremy/Wasted.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func Wasted(personnage.PV_Actuels) {
+func Wasted(Personnage.PV_Actuels) {
 
-	if personnage.PV_Actuels <= 0 {
+	if Personnage.PV_Actuels <= 0 {
 		fmt.Println("Vous avez perdu !")
 	}
 
